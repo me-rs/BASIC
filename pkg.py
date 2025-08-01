beccfb2f07e5e7deeb98624ad4f1e5031bf8afae
MD RIAZ SARDER <mdriazsarder00@gmail.com>
2025-08-01 23:42:44 +0600

    Add files via upload

---
+++ pkg.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import time
+os.system("clear")
+print("\n\n\n")
+ab=("		System Loading...")
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print("\n\n\n")
+time.sleep(2)
+os.system("clear")
+print("\n\n")
+ab=("	\033[1;32mSuccessfully loaded\n")
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+name = input("	\033[1;34mYour name: ")
+ab="	\033[1;33mHey \033[1;32m"+name+"\033[1;33m, Be Ethical..."
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print("\n\n")
+time.sleep(2)
+os.system("clear")
+print("""\033[1;36m
+
+  ____________________.____   __________ 
+ /   _____/\\__    ___/|    |  \\______   \\
+ \\_____  \\   |    |   |    |   |     ___/
+ /        \\  |    |   |    |___|    |    
+/_______  /  |____|   |_______ \\____|    
+        \\/                    \\/         
+
+\033[1;33m=================================\033[1;32m
+Owner	: STLP TEAM
+Github	: something
+Facebook: STLP
+Use only for educational purposes
+\033[1;33m=================================\033[0m
+""")
+print("\033[1;36m")
+os.system("pkg up")
+print("\033[1;33m")
+os.system("pkg install git")
+print("\033[1;31m")
+os.system("pkg install ruby")
+print("\033[1;34m")
+os.system("gem install lolcat")
+print("\033[1;32m")
+os.system("pip install requests")
+print("\033[1;36m")
+os.system("pkg install curl")
+print("\033[1;33m")
+os.system("pkg install tsu")
+print("\033[1;31m")
+os.system("pkg install nodejs")
+print("\033[1;34m")
+os.system("pkg install wget")
+print("\033[1;32m")
+os.system("pkg install nano")
+print("\033[1;36m")
+os.system("pkg install perl")
+print("\033[1;33m")
+os.system("pkg install nmap")
+print("\033[1;31m")
+os.system("pkg install bash")
+print("\033[1;34m")
+os.system("pkg install clang")
+print("\033[1;32m")
+os.system("pkg install help")
+print("\033[1;36m")
+os.system("pkg install w3m")
+print("\033[1;33m")
+os.system("pkg install figlet")
+print("\033[1;31m")
+os.system("pkg install zip")
+print("\033[1;34m")
+os.system("pkg install unzip")
+print("\033[1;32m")
+os.system("pkg install fish")
+print("\033[1;36m")
+os.system("pkg install cowsay")
+print("\033[1;33m")
+os.system("pkg install hydra")
+print("\033[1;31m")
+os.system("pkg install wcalc")
+print("\033[1;34m")
+os.system("pkg install golang")
+print("\033[1;33m")
+os.system("pkg install toilet")
+
+print("\033[1;36m")
+print("All package installef successfully!")
