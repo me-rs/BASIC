b65daf62f2b068e37527bfd1919f5d5326885b20
MD RIAZ SARDER <mdriazsarder00@gmail.com>
2025-08-02 12:39:12 +0600

    Update pkg.py

--- pkg.py
+++ pkg.py
@@ -17,31 +17,54 @@ for c in ab:
 	sys.stdout.write(c)
 	sys.stdout.flush()
 	time.sleep(0.1)
-name = input("	\033[1;34mYour name: ")
-ab="	\033[1;33mHey \033[1;32m"+name+"\033[1;33m, Be Ethical..."
-for c in ab:
+import os
+import sys
+import time
+os.system("clear")
+time.sleep(1)
+print("""
+        \033[1;32m               
+RRRRRRRRRRRRRRRRR      SSSSSSSSSSSSSSS 
+R::::::::::::::::R   SS:::::::::::::::S
+R::::::RRRRRR:::::R S:::::SSSSSS::::::S
+RR:::::R     R:::::RS:::::S     SSSSSSS
+  R::::R     R:::::RS:::::S            
+  R::::R     R:::::RS:::::S            
+  R::::RRRRRR:::::R  S::::SSSS         
+  R:::::::::::::RR    SS::::::SSSSS    
+  R::::RRRRRR:::::R     SSS::::::::SS  
+  R::::R     R:::::R       SSSSSS::::S 
+  R::::R     R:::::R            S:::::S
+  R::::R     R:::::R            S:::::S
+RR:::::R     R:::::RSSSSSSS     S:::::S
+R::::::R     R:::::RS::::::SSSSSS:::::S
+R::::::R     R:::::RS:::::::::::::::SS 
+RRRRRRRR     RRRRRRR SSSSSSSSSSSSSSS   
+ 
+""")
+
+m="——————————————————————————————————————\n\033[1;32mM   U   L   T   I   M   E   D   I   A\n——————————————————————————————————————\033[0m"
+for c in m:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.01)
+time.sleep(2)
+print("\n\n")
+a="Welcome to \033[1;32mRS Multimedia"
+for c in a:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.2)
+time.sleep(2)
+n=input("\n\033[1;35mYour name: \033[1;34m")
+a="\033[33mHello  \033[1;34m "+n+"\033[33m, Be Ethocal & stay with us.\n"
+for c in a:
 	sys.stdout.write(c)
 	sys.stdout.flush()
 	time.sleep(0.1)
 print("\n\n")
 time.sleep(2)
 os.system("clear")
-print("""\033[1;36m
-
-  ____________________.____   __________ 
- /   _____/\\__    ___/|    |  \\______   \\
- \\_____  \\   |    |   |    |   |     ___/
- /        \\  |    |   |    |___|    |    
-/_______  /  |____|   |_______ \\____|    
-        \\/                    \\/         
-
-\033[1;33m=================================\033[1;32m
-Owner	: STLP TEAM
-Github	: something
-Facebook: STLP
-Use only for educational purposes
-\033[1;33m=================================\033[0m
-""")
 print("\033[1;36m")
 os.system("pkg up")
 print("\033[1;33m")
@@ -94,4 +117,4 @@ print("\033[1;33m")
 os.system("pkg install toilet")
 
 print("\033[1;36m")
-print("All package installef successfully!")
+print("All package installef successfully!"
